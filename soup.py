4f002915cbec3f9521b3d4996ac87d62fe674870
Takayuki Asaka <keroringo@gmail.com>
2018-12-02 00:34:49 +0900

    変更内容を簡潔にコメント

---
+++ soup.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from requests_oauthlib import OAuth1Session
+import json
+import os
+import datetime
+
+headers = {'User-Agent':'Mozilla/5.0'}
+
+url = 'https://www.cyclocross.jp/news/'
+soup = BeautifulSoup(requests.get(url).content,'html.parser')
+
+h2 = soup.html.h2
+print(h2.string)
+a = soup.find("p", class_="click")
+a = a.find("a")
+a = a.attrs['href']
+print(a)
+
+twitter = OAuth1Session(os.environ["CONSUMER_KEY"],  os.environ["CONSUMER_SECRET"], os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"])
+
+tweet = h2.string + a
+params = {"status": tweet}
+
+req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params = params)
+
